481bb2356b2dabf2a74ad2c7113ec57e3d68d69b
Mohamed Ibrahim <55456401+mohamed8270@users.noreply.github.com>
2024-09-28 00:12:14 +0530

    API constants updated with values

--- lib/utils/constants/api_constants.dart
+++ lib/utils/constants/api_constants.dart
@@ -1,9 +1,10 @@
 // list of constant API will be here
 
 class ApiConstants {
-  static const String baseUrl = '';
-  static const String productAdd = '';
-  static const String productGetAll = '';
-  static const String productGetDetails = '';
-  static const String productSimilar = '';
+  // API url's
+  static const String baseUrl = 'https://server-one-orpin.vercel.app/';
+  static const String productAdd = '${baseUrl}products/email';
+  static const String productGetAll = '${baseUrl}products';
+  static const String productGetDetails = '${baseUrl}products/details/';
+  static const String productSimilar = '${baseUrl}products/similar/product/';
 }
